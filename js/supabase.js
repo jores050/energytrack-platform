311a6626dcd42314d950a005268c76d1de5780dc
jores050 <144858952+jores050@users.noreply.github.com>
2026-05-12 15:59:39 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const supabaseUrl='https://prvsatpdgybnevolzhhf.supabase.co';
+
+const supabaseKey='TA_CLE_PUBLIQUE_SUPABASE';
+
+const supabaseClient=supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
